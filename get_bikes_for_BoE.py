e6dce4ad686248569a6f24f4908c3dc8dd6b1f08
Himasou Makwana <himasoum@virtusa.com>
2021-03-28 14:11:59 +0100

    fix typo

--- get_bikes_for_BoE.py
+++ get_bikes_for_BoE.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 import requests
 import json
-#The Bike point loction for Bank of England is BikePoints_340"
+#The Bike point location for Bank of England is BikePoints_340"
 urllink = "https://api.tfl.gov.uk/BikePoint/BikePoints_340"
 resp = requests.get(urllink)
 python_data = (json.loads(resp.text))['additionalProperties']
